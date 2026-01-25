f0fa7d73877f6c5e9bcab9cce3ea1351e61578b2
anhnhatdev <anhphanlenhat@gmail.com>
2026-01-25 13:41:22 +0700

    test(platform): execute and verify test suites across all 7 microservices

--- inventory-service/mvnw.cmd
+++ inventory-service/mvnw.cmd
@@ -1,4 +1,4 @@
 @echo off
 setlocal
-set MAVEN_HOME=C:\Users\anh\.m2\wrapper\dists\apache-maven-3.9.6-bin\apache-maven-3.9.6
-"%MAVEN_HOME%\bin\mvn.cmd" %*
+set "MAVEN_HOME=C:\Users\anh\.m2\wrapper\dists\apache-maven-3.9.6-bin\apache-maven-3.9.6"
+java "-Dmaven.home=%MAVEN_HOME%" "-Dclassworlds.conf=%MAVEN_HOME%\bin\m2.conf" "-Dmaven.multiModuleProjectDirectory=%CD%" -classpath "%MAVEN_HOME%\boot\plexus-classworlds-2.7.0.jar" org.codehaus.plexus.classworlds.launcher.Launcher %*
